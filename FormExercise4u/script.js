52d74c550392574c056eb5d9341a3df1028b8803
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-08-17 20:04:56 +0800

    Update script.js

--- FormExercise4u/script.js
+++ FormExercise4u/script.js
@@ -1,16 +1,19 @@
+// Reference form HTML Elements
 const form = document.querySelector('form')
 const Product_Name = document.getElementById('product');
 const Product_QTY = document.getElementById('qty');
 const list = document.getElementById('list')
 
-
+// Declaration of Arrow Function
+// Add a submit event listener to the form
 form.addEventListener('submit', (element) => {
-    element.preventDefault();
+    
+    element.preventDefault(); // Prevent default behavior of Form submission
 
-    if (!Product_Name.value && !Product_QTY.value) return;
+    if (!Product_Name.value && !Product_QTY.value) return; // To check if inputs are empty
 
-    const newLI = document.getElementById('list');
-    newLI.textContent = `${Product_QTY.value} ${Product_Name.value}`;
+    const newLI = document.getElementById('list'); // Calls the list from HTML Element
+    newLI.textContent = `${Product_QTY.value} ${Product_Name.value}`; // Format the values
     list.appendChild(newLI)
 
     Product_Name.value = ''
